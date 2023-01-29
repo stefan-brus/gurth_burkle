861007360527a14e751da8f0382e3d863dd13fb8
Stefan Brus <stefan1brus@gmail.com>
2023-01-29 21:44:24 +0100

    Change build out dir

--- build.mjs
+++ build.mjs
@@ -15,7 +15,7 @@ build({
   target: "rhino1.7.14",
   external: ["kolmafia"],
   plugins: [babel()],
-  outdir: "dist/scripts/gurth_burkle",
+  outdir: "../../scripts/gurth_burkle",
   watch,
   loader: { ".json": "text" },
   inject: ["./kolmafia-polyfill.js"],
