b479786feffd21eb308a81031c1921346a701523
Stefan Brus <stefan1brus@gmail.com>
2023-02-04 00:32:30 +0100

    Add some TT gear

--- src/gear/GearTurtleTamer.ts
+++ src/gear/GearTurtleTamer.ts
@@ -1,4 +1,4 @@
-import { equip, equippedItem, Item, Slot, userConfirm } from "kolmafia";
+import { canEquip, equip, equippedItem, Item, Slot, userConfirm } from "kolmafia";
 import { $item, $slot } from "libram";
 import { chooseItemFromPriorityList } from "./Utils";
 
@@ -14,14 +14,17 @@ export function selectTurtleTamerGear(reservedSlots: Slot[]) {
 }
 
 const TurtleTamerWeapons: Item[] = [
+  $item`Knob Goblin scimitar`,
   $item`turtle totem`,
 ];
 
 const TurtleTamerHats: Item[] = [
+  $item`chef's hat`,
   $item`helmet turtle`,
 ];
 
 const TurtleTamerPants: Item[] = [
+  $item`Knob Goblin pants`,
   $item`old sweatpants`,
 ];
 
@@ -29,7 +32,7 @@ function selectForSlot(slot: Slot, items: Item[]) {
   const curItem = equippedItem(slot);
   const newItem = chooseItemFromPriorityList(items, curItem);
 
-  if (newItem !== curItem) {
+  if (newItem !== curItem && canEquip(newItem)) {
     if (userConfirm(`Equip ${newItem} to slot ${slot.toString()}?`)) {
       equip(slot, newItem);
     }
