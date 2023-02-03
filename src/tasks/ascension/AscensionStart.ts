d6199639452f79cd33e980cc01fb830c094cb743
Stefan Brus <stefan1brus@gmail.com>
2023-02-03 19:37:16 +0100

    Add get saucepan task

--- src/tasks/ascension/AscensionStart.ts
+++ src/tasks/ascension/AscensionStart.ts
@@ -1,4 +1,5 @@
 import { adv1, autosell, buy, Class, eat, getProperty, Item, itemAmount, Location, myClass, myMeat, runChoice, use, visitUrl } from "kolmafia";
+import { $item } from "libram";
 import { Constants } from "../../Constants";
 import { Task } from "../Task";
 
@@ -13,30 +14,36 @@ export const AscensionStartTask: Task = {
     },
     {
       name: "Read King Ralph's letter",
-      available: () => itemAmount(Item.get("letter from King Ralph XI")) > 0,
-      progress: () => { use(1, Item.get("letter from King Ralph XI")); },
+      available: () => itemAmount($item`letter from King Ralph XI`) > 0,
+      progress: () => { use(1, $item`letter from King Ralph XI`); },
       completed: () => getProperty(TootOrioleQuestProperty) === "finished" && 
-        itemAmount(Item.get("letter from King Ralph XI")) < 1,
+        itemAmount($item`letter from King Ralph XI`) < 1,
     },
     {
       name: "Sell pork elf goodies",
-      available: () => itemAmount(Item.get("pork elf goodies sack")) > 0,
+      available: () => itemAmount($item`pork elf goodies sack`) > 0,
       progress: () => sellPorkElfGoodies(),
       completed: () => getProperty(TootOrioleQuestProperty) === "finished" && 
-        itemAmount(Item.get("letter from King Ralph XI")) < 1 && 
-        itemAmount(Item.get("pork elf goodies sack")) < 1,
+        itemAmount($item`letter from King Ralph XI`) < 1 && 
+        itemAmount($item`pork elf goodies sack`) < 1,
     },
     {
       name: "Buy antique accordion",
-      available: () => myClass() != Class.get("Accordion Thief") && itemAmount(Item.get("antique accordion")) < 1 && myMeat() >= 2500,
-      progress: () => { buy(1, Item.get("antique accordion")); },
-      completed: () => itemAmount(Item.get("antique accordion")) > 0,
+      available: () => myClass() != Class.get("Accordion Thief") && itemAmount($item`antique accordion`) < 1 && myMeat() >= 2500,
+      progress: () => { buy(1, $item`antique accordion`); },
+      completed: () => itemAmount($item`antique accordion`) > 0,
+    },
+    {
+      name: "Acquire saucepan",
+      available: () => itemAmount($item`saucepan`) < 1,
+      progress: () => getChewinggumItem($item`saucepan`),
+      completed: () => itemAmount($item`saucepan`) > 0,
     },
     {
       name: "Deploy tent",
-      available: () => itemAmount(Item.get("newbiesport&trade; tent")) > 0,
-      progress: () => { use(1, Item.get("newbiesport&trade; tent")); },
-      completed: () => itemAmount(Item.get("newbiesport&trade; tent")) < 1,
+      available: () => itemAmount($item`newbiesport&trade; tent`) > 0,
+      progress: () => { use(1, $item`newbiesport&trade; tent`); },
+      completed: () => itemAmount($item`newbiesport&trade; tent`) < 1,
     },
     {
       name: "Start Doc Galaktik's quest",
@@ -52,9 +59,9 @@ export const AscensionStartTask: Task = {
     },
     {
       name: "Eat a hotdog",
-      available: () => itemAmount(Item.get("astral hot dog")) === 3,
-      progress: () => { eat(1, Item.get("astral hot dog")); },
-      completed: () => itemAmount(Item.get("astral hot dog")) < 3,
+      available: () => itemAmount($item`astral hot dog`) === 3,
+      progress: () => { eat(1, $item`astral hot dog`); },
+      completed: () => itemAmount($item`astral hot dog`) < 3,
     },
   ],
 };
@@ -63,7 +70,7 @@ const TootOrioleQuestProperty = "questM05Toot";
 const DocGalaktikQuestProperty = "questM24Doc";
 
 function sellPorkElfGoodies() {
-  use(1, Item.get("pork elf goodies sack"));
+  use(1, $item`pork elf goodies sack`);
 
   Item.get(["hamethyst", "baconstone", "porquoise"]).forEach(item => {
     autosell(itemAmount(item), item);
@@ -97,4 +104,16 @@ function deployShinies() {
 
   // Visit workshed to refresh kolmafia trainset properties
   visitUrl("campground.php?action=workshed");
+}
+
+function getChewinggumItem(item: Item) {
+  while (itemAmount(item) < 1) {
+    if (itemAmount($item`chewing gum on a string`) < 1 && !buy(1, $item`chewing gum on a string`)) {
+      throw new Error("Unable to buy chewing gum on a string");
+    }
+
+    if (!use(1, $item`chewing gum on a string`)) {
+      throw new Error("Unable to use chewing gum on a string");
+    }
+  }
 }
