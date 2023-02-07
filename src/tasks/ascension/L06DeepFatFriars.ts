6eb487d5163aaae1873fa26ce3945920b72d7f61
Stefan Brus <stefan1brus@gmail.com>
2023-02-07 16:41:39 +0100

    Add mainstat to friars tasks

--- src/tasks/ascension/L06DeepFatFriars.ts
+++ src/tasks/ascension/L06DeepFatFriars.ts
@@ -17,18 +17,21 @@ export const L06Task: Task = {
       available: () => getProperty(L06QuestProperty) === "step1" && itemAmount($item`dodecagram`) < 1,
       progress: () => { return { location: $location`The Dark Neck of the Woods`, modifiers: [Modifier.NonCombat] }; },
       completed: () => itemAmount($item`dodecagram`) > 0,
+      mainstat: $location`The Dark Neck of the Woods`.recommendedStat,
     },
     {
       name: "Get box of birthday candles",
       available: () => getProperty(L06QuestProperty) === "step1" && itemAmount($item`box of birthday candles`) < 1,
       progress: () => { return { location: $location`The Dark Heart of the Woods`, modifiers: [Modifier.NonCombat] }; },
       completed: () => itemAmount($item`box of birthday candles`) > 0,
+      mainstat: $location`The Dark Heart of the Woods`.recommendedStat,
     },
     {
       name: "Get eldritch butterknife",
       available: () => getProperty(L06QuestProperty) === "step1" && itemAmount($item`eldritch butterknife`) < 1,
       progress: () => { return { location: $location`The Dark Elbow of the Woods`, modifiers: [Modifier.NonCombat] }; },
       completed: () => itemAmount($item`eldritch butterknife`) > 0,
+      mainstat: $location`The Dark Elbow of the Woods`.recommendedStat,
     },
     {
       name: "Cleanse the taint",
