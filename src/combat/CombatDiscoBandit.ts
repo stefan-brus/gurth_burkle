af0e713747592bb27e82d908b1a921d04af25e9d
Stefan Brus <stefan1brus@gmail.com>
2023-02-14 21:24:02 +0100

    Add disco dance combat logic

--- src/combat/CombatDiscoBandit.ts
+++ src/combat/CombatDiscoBandit.ts
@@ -1,5 +1,5 @@
-import { attack, Monster, steal, throwItem } from "kolmafia";
-import { $item } from "libram";
+import { attack, haveSkill, Monster, steal, throwItem, useSkill, willUsuallyMiss } from "kolmafia";
+import { $item, $skill } from "libram";
 import { combatOver, shouldThrowFlyers } from "./Utils";
 
 export function consultDiscoBandit(initRound: number, foe: Monster, page: string) {
@@ -14,11 +14,17 @@ export function consultDiscoBandit(initRound: number, foe: Monster, page: string
 type CombatState = {
   pickpocketed: boolean,
   flyered: boolean,
+  dance1: boolean,
+  dance2: boolean,
+  dance3: boolean,
 };
 
 const InitState: CombatState = {
   pickpocketed: false,
   flyered: false,
+  dance1: false,
+  dance2: false,
+  dance3: false,
 };
 
 function doRound(foe: Monster, state: CombatState): [string, CombatState] {
@@ -32,6 +38,23 @@ function doRound(foe: Monster, state: CombatState): [string, CombatState] {
   else if (shouldThrowFlyers() && !state.flyered) {
     newState.flyered = true;
     resultPage = throwItem($item`rock band flyers`);
+  }
+  else if (willUsuallyMiss()) {
+    if (!state.dance1 && haveSkill($skill`Disco Dance of Doom`)) {
+      newState.dance1 = true;
+      resultPage = useSkill($skill`Disco Dance of Doom`);
+    }
+    else if (!state.dance2 && haveSkill($skill`Disco Dance II: Electric Boogaloo`)) {
+      newState.dance2 = true;
+      resultPage = useSkill($skill`Disco Dance II: Electric Boogaloo`);
+    }
+    else if (!state.dance3 && haveSkill($skill`Disco Dance 3: Back in the Habit`)) {
+      newState.dance3 = true;
+      resultPage = useSkill($skill`Disco Dance 3: Back in the Habit`);
+    }
+    else {
+      resultPage = attack();
+    }
   }
   else {
     resultPage = attack();
