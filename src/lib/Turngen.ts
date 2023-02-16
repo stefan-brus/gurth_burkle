f8eb7720499bd6c0eceb72979cdd049b60fbed0a
Stefan Brus <stefan1brus@gmail.com>
2023-02-16 20:09:55 +0100

    Try to use nash crosby's still

--- src/lib/Turngen.ts
+++ src/lib/Turngen.ts
@@ -1,4 +1,4 @@
-import { buy, create, drink, eat, getProperty, haveEffect, Item, itemAmount, mpCost, myAdventures, myInebriety, myLevel, myMaxmp, myPrimestat, use, userConfirm, useSkill } from "kolmafia";
+import { buy, create, drink, eat, getProperty, haveEffect, Item, itemAmount, mpCost, myAdventures, myInebriety, myLevel, myMaxmp, myPrimestat, stillsAvailable, use, userConfirm, useSkill } from "kolmafia";
 import { $coinmaster, $effect, $item, $skill, $stat } from "libram";
 import { cookCbbFoods } from "../shinies/Cookbookbat";
 import { billiardsRoomDone } from "../tasks/ascension/Spookyraven";
@@ -97,8 +97,10 @@ function generateLiver() {
   const inebrietyAvailable = BoozePriority.reduce<number>((acc, item) => acc + item.inebriety * itemAmount(item), 0);
   let inebrietyCreated = 0;
 
+  use(itemAmount($item`booze bindle`), $item`booze bindle`);
+
   if (myPrimestat === $stat`Moxie`) {
-    inebrietyCreated += tryCreateDrinks(ExtraFruityDrinks, liverRemaining() - inebrietyAvailable - inebrietyCreated);
+    inebrietyCreated += tryCreateMoxieDrinks(ExtraFruityDrinks, liverRemaining() - inebrietyAvailable - inebrietyCreated);
   }
 
   inebrietyCreated += tryCreateDrinks(FruityGirlDrinks, liverRemaining() - inebrietyAvailable - inebrietyCreated);
@@ -112,16 +114,90 @@ function generateLiver() {
   }
 }
 
+const DrinkIngredients: Map<Item, Item[]> = new Map([
+  [$item`Divine`, [$item`bottle of Domesticated Turkey`, $item`kumquat`]],
+  [$item`gimlet`, [$item`bottle of Calcutta Emerald`, $item`tonic water`]],
+  [$item`Gordon Bennett`, [$item`boxed champagne`, $item`kumquat`]],
+  [$item`Mae West`, [$item`bottle of Domesticated Turkey`, $item`raspberry`]],
+  [$item`mandarina colada`, [$item`bottle of Lieutenant Freeman`, $item`tangerine`]],
+  [$item`Mon Tiki`, [$item`bottle of Lieutenant Freeman`, $item`kiwi`]],
+  [$item`Neuromancer`, [$item`bottle of Calcutta Emerald`, $item`cocktail onion`]],
+  [$item`prussian cathouse`, [$item`boxed Champagne`, $item`raspberry`]],
+  [$item`tangarita`, [$item`bottle of Jorge Sinsonte`, $item`tangerine`]],
+  [$item`teqiwila slammer`, [$item`bottle of Jorge Sinsonte`, $item`kiwi`]],
+  [$item`vodka stratocaster`, [$item`bottle of Definit`, $item`cocktail onion`]],
+  [$item`yellow brick road`, [$item`bottle of Definit`, $item`tonic water`]],
+]);
+
+const StillIngredients: Map<Item, Item> = new Map([
+  // Alcohol
+  [$item`bottle of Calcutta Emerald`, $item`bottle of gin`],
+  [$item`bottle of Lieutenant Freeman`, $item`bottle of rum`],
+  [$item`bottle of Jorge Sinsonte`, $item`bottle of tequila`],
+  [$item`bottle of Definit`, $item`bottle of vodka`],
+  [$item`bottle of Domesticated Turkey`, $item`bottle of whiskey`],
+  [$item`boxed champagne`, $item`boxed wine`],
+  [$item`bottle of Ooze-O`, $item`bottle of sewage schnapps`],
+  [$item`bottle of Pete's Sake`, $item`bottle of sake`],
+
+  // Mixer
+  [$item`tangerine`, $item`grapefruit`],
+  [$item`kiwi`, $item`lemon`],
+  [$item`cocktail onion`, $item`olive`],
+  [$item`kumquat`, $item`orange`],
+  [$item`tonic water`, $item`soda water`],
+  [$item`raspberry`, $item`strawberry`],
+]);
+
+function getStillItems(alcohol: Item, mixer: Item): [Item, Item] {
+  const alcoholItem = StillIngredients.get(alcohol);
+  if (alcoholItem === undefined)
+    throw new Error("Unknown still alcohol: " + alcohol.name);
+
+  const mixerItem = StillIngredients.get(mixer);
+  if (mixerItem === undefined)
+    throw new Error("Unknown still mixer: " + mixer.name);
+  
+  return [alcoholItem, mixerItem];
+}
+
+function tryCreateMoxieDrinks(drinks: Item[], inebriety: number): number {
+  let inebrietyCreated = 0;
+
+  for (const drink of drinks) {
+    if (inebrietyCreated >= inebriety) {
+      break;
+    }
+
+    const ingredients = DrinkIngredients.get(drink);
+    if (ingredients === undefined)
+      throw new Error("Unknow still drink: " + drink.name);
+    const [alcohol, mixer] = ingredients;
+    const [alcoholItem, mixerItem] = getStillItems(alcohol, mixer);
+
+    while (stillsAvailable() >= 2 && itemAmount(alcoholItem) > 0 && itemAmount(mixerItem) > 0) {
+      create(1, alcohol);
+      create(1, mixer);
+    }
+
+    while (haveIngredients(drink) && inebrietyCreated < inebriety && create(1, drink)) {
+      inebrietyCreated += drink.inebriety;
+    }
+  }
+
+  return inebrietyCreated;
+}
+
 function tryCreateDrinks(drinks: Item[], inebriety: number): number {
   let inebrietyCreated = 0;
 
   for (const drink of drinks) {
-    while (haveIngredients(drink) && inebrietyCreated < inebriety && create(1, drink)) {
-      inebrietyCreated += drink.inebriety;
-    }
-
     if (inebrietyCreated >= inebriety) {
       break;
+    }
+
+    while (haveIngredients(drink) && inebrietyCreated < inebriety && create(1, drink)) {
+      inebrietyCreated += drink.inebriety;
     }
   }
 
