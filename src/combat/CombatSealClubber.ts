a8c778a2f370425f60418de40769428d2f22ab8b
Stefan Brus <stefan1brus@gmail.com>
2023-03-16 20:13:35 +0100

    Add seal clubber combat script

---
+++ src/combat/CombatSealClubber.ts
@@ -0,0 +1,58 @@
+import { attack, haveSkill, Monster, monsterHp, myFury, throwItem, useSkill, willUsuallyMiss } from "kolmafia";
+import { $item, $skill } from "libram";
+import { combatLoop, ImportantFoes, shouldThrowFlyers } from "./Utils";
+
+export function consultSealClubber(initRound: number, foe: Monster, page: string) {
+  combatLoop(foe, page, doRound, InitState);
+}
+
+type CombatState = {
+  flyered: boolean,
+};
+
+const InitState: CombatState = {
+  flyered: false,
+};
+
+function doRound(foe: Monster, state: CombatState): [string, CombatState] {
+  let newState = { ...state };
+  let resultPage = "";
+
+  if (shouldThrowFlyers() && !state.flyered) {
+    newState.flyered = true;
+    resultPage = throwItem($item`rock band flyers`);
+  }
+  else if (shouldBatterUp(foe)) {
+    resultPage = useSkill($skill`Batter Up!`);
+  }
+  else if (willUsuallyMiss()) {
+    resultPage = useSkill($skill`Cannelloni Cannon`);
+  }
+  else if (haveSkill($skill`Lunging Thrust-Smack`) && monsterHp() > 250) {
+    resultPage = useSkill($skill`Lunging Thrust-Smack`);
+  }
+  else if (haveSkill($skill`Thrust-Smack`) && monsterHp() > 100) {
+    resultPage = useSkill($skill`Thrust-Smack`);
+  }
+  else {
+    resultPage = attack();
+  }
+
+  return [resultPage, newState];
+}
+
+function shouldBatterUp(foe: Monster): boolean {
+  if (!haveSkill($skill`Batter Up!`) || myFury() < 5) {
+    return false;
+  }
+
+  if (foe.boss) {
+    return false;
+  }
+
+  if (ImportantFoes.includes(foe)) {
+    return false;
+  }
+
+  return true;
+}
--- src/consult.ts
+++ src/consult.ts
@@ -4,10 +4,14 @@ import { consultAccordionThief } from "./combat/CombatAccordionThief";
 import { consultDiscoBandit } from "./combat/CombatDiscoBandit";
 import { consultPastamancer } from "./combat/CombatPastamancer";
 import { consultSauceror } from "./combat/CombatSauceror";
+import { consultSealClubber } from "./combat/CombatSealClubber";
 import { consultTurtleTamer } from "./combat/CombatTurtleTamer";
 
 export function main(initRound: number, foe: Monster, page: string) {
   switch (myClass()) {
+    case $class`Seal Clubber`:
+      consultSealClubber(initRound, foe, page);
+      break;
     case $class`Turtle Tamer`:
       consultTurtleTamer(initRound, foe, page);
       break;
