3d24917ccfaedf203ad2016c274a23806444b624
Stefan Brus <stefan1brus@gmail.com>
2023-05-16 19:31:20 +0200

    Fix king ralph URL

--- src/gb/task/standard/Level13.ts
+++ src/gb/task/standard/Level13.ts
@@ -4,18 +4,6 @@ import { Action } from "../../action/Action";
 import { ActionsFactory } from "../../action/Factory";
 import { $location } from "libram";
 
-export const FreeKingRalph: Task = {
-  name: "Free King Ralph",
-  actions: () => { throw "TODO: Ralph URL" },
-  completed: () => isKingLiberated(),
-  requires: [],
-};
-
-function isKingLiberated(): boolean {
-  const KingLiberatedProperty = "kingLiberated";
-  return getProperty(KingLiberatedProperty) === "true";
-}
-
 const KillNaughtySorceress: Task = {
   name: "Kill the naughty sorceress",
   actions: () => killNSActions(),
@@ -38,4 +26,25 @@ function isSorceressDead(): boolean {
   const Values = ["step12", "step13", "finished"];
 
   return Values.includes(getProperty(L13Property));
+}
+
+export const FreeKingRalph: Task = {
+  name: "Free King Ralph",
+  actions: () => freeKingActions(),
+  completed: () => isKingLiberated(),
+  requires: [KillNaughtySorceress],
+};
+
+function freeKingActions(): Action[] {
+  const FreeKingAction = ActionsFactory.urlAction(
+    "Free King Ralph",
+    "place.php?whichplace=nstower&action=ns_11_prism",
+  );
+
+  return [FreeKingAction];
+}
+
+function isKingLiberated(): boolean {
+  const KingLiberatedProperty = "kingLiberated";
+  return getProperty(KingLiberatedProperty) === "true";
 }
